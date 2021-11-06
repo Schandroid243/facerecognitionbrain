d0c22eb81e937c860095f23c5eeaa9cdcfe18c62
Schandroid243 <schadrackngunza@gmail.com>
2021-11-06 09:33:06 +0100

    Added a bit of syntactic sugar when refactoring Navigation component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,21 +2,25 @@ import React from 'react';
 
 
 const Navigation = ({ onRouteChange, isSignedIin }) => {
-        if(isSignedIin) {
-            return( 
-                <nav style={{display: 'flex', justifyContent:'flex-end', padding:10}}>
+
+    const signOut = () => 
+         (
+            <nav style={{display: 'flex', justifyContent:'flex-end', padding:10}}>
                 <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'> Sign Out </p>
-                </nav>
-            )
-            
-        } else {
-            return(
-                <nav style={{display: 'flex', justifyContent:'flex-end', padding:10}}>
-                <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'> Sign In </p>
-                <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'> Register </p>
-                </nav>
-            )
-        }
+            </nav>
+        )
+    
+
+    const signIn = () => 
+        (
+            <nav style={{display: 'flex', justifyContent:'flex-end', padding:10}}>
+            <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'> Sign In </p>
+            <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'> Register </p>
+            </nav>
+        )
+    
+
+    return isSignedIin ? signOut() :signIn();
 }
 
 export default Navigation;
